7d51b03139209efda82efe79034a68703980a595
edp1096 <edp1096@naver.com>
2022-08-31 14:47:52 +0900

    Modify build html

--- esbuild-scripts/esbuild.html.js
+++ esbuild-scripts/esbuild.html.js
@@ -2,10 +2,12 @@ import fs from "fs"
 
 fs.readdirSync("src/html").forEach((fname) => {
     if (fs.lstatSync("src/html/" + fname).isDirectory()) {
-        const dname = fname
-        if (!fs.existsSync("dist/" + dname)) { fs.mkdirSync("dist/" + dname) }
-        fs.readdirSync("src/html/" + dname).forEach((fname) => {
-            fs.copyFileSync(`src/html/${dname}/${fname}`, `dist/${dname}/${fname}`)
+        const dirName = fname
+        if (!fs.existsSync("dist/" + dirName)) { fs.mkdirSync("dist/" + dirName) }
+
+        const dirChildren = fs.readdirSync("src/html/" + dirName)
+        dirChildren.forEach((fname) => {
+            fs.copyFileSync(`src/html/${dirName}/${fname}`, `dist/${dirName}/${fname}`)
         })
     } else {
         fs.copyFileSync(`src/html/${fname}`, `dist/${fname}`)
